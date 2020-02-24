95d36b4b3116dd07cb1b9f23b8cc59e591bc3f47
Kaan <kaanaltinay00@gmail.com>
2020-02-24 20:58:13 +0300

    Add Colors.js file to use in styling

--- constants/Colors.js
+++ constants/Colors.js
@@ -1,14 +1,10 @@
-const tintColor = '#2f95dc';
+
 
 export default {
-  tintColor,
-  tabIconDefault: '#ccc',
-  tabIconSelected: tintColor,
-  tabBar: '#fefefe',
-  errorBackground: 'red',
-  errorText: '#fff',
-  warningBackground: '#EAEB5E',
-  warningText: '#666804',
-  noticeBackground: tintColor,
-  noticeText: '#fff',
+  menuBarColor: '#D5D8DC',
+  listTitleColor: '#FFC300',
+  listSubtitleColor: '#DAF7A6',
+  textColor: '#2B2727',
+  listItemBackgroundColor: '#566573',
+  bgColor: '#808996',
 };
