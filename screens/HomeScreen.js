e46948a212c1604cdd3867cf71861fa62ac48952
Kaan <kaanaltinay00@gmail.com>
2020-02-25 00:57:03 +0300

    Fix camera behavior

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -7,23 +7,51 @@ import {
   View,
 } from 'react-native'
 import Colors from "../constants/Colors"
-
+import { Camera } from 'expo-camera';
+import * as Permissions from 'expo-permissions';
+import {FontAwesome, Ionicons, MaterialCommunityIcons} from '@expo/vector-icons';
 
 export default class HomeScreen extends Component {
   constructor(props) {
     super(props);
-    this.state = { count: 0 }
+    this.state = {
+      cameraOpened: false,
+      hasPermission: null,
+      type: Camera.Constants.Type.back,
+    }
+  }
+
+  async componentDidMount() {
+    const { status } = await Permissions.askAsync(Permissions.CAMERA);
+    this.setState({ hasPermission: status === 'granted' });
   }
 
   onPress = () => {
-    this.setState({
-      count: 1
+    this.setState((prevState) => ({
+      cameraOpened: !prevState.cameraOpened
+    }));
+  };
+
+  handleCameraType=()=>{
+    const { cameraType } = this.state
+
+    this.setState({cameraType:
+          cameraType === Camera.Constants.Type.back
+              ? Camera.Constants.Type.front
+              : Camera.Constants.Type.back
     })
+  };
+
+  takePicture = async () => {
+    if (this.camera) {
+      let photo = await this.camera.takePictureAsync();
+      console.log(photo);
+    }
   };
 
   render() {
     return (
-        this.state.count === 0 ?
+        this.state.cameraOpened === false ?
         <View style={styles.container}>
           <TouchableOpacity
               style={styles.button}
@@ -31,13 +59,55 @@ export default class HomeScreen extends Component {
           >
             <Image source={require('../assets/images/no-bg.png')}  style={styles.img}/>
           </TouchableOpacity >
-          <View style={[styles.countContainer]}>
-            <Text style={[styles.countText]}>
-              { this.state.count !== 0 ? this.state.count: null}
-            </Text>
-          </View>
         </View> :
-        null
+        <View style={{ flex: 1 }}>
+          <Camera
+            style={{ flex: 1 }}
+            type={this.state.cameraType}
+            ref={ref => {
+              this.camera = ref;
+            }}
+          >
+            <View style={{flex:1, flexDirection:"row",justifyContent:"space-between",margin:20}}>
+              <TouchableOpacity
+                  style={{
+                    alignSelf: 'flex-end',
+                    alignItems: 'center',
+                    backgroundColor: 'transparent',
+                  }}
+                onPress={()=>this.handleCameraType()}>
+                <MaterialCommunityIcons
+                    name="camera-switch"
+                    style={{ color: "#fff", fontSize: 40}}
+                />
+              </TouchableOpacity>
+              <TouchableOpacity
+                  style={{
+                    alignSelf: 'flex-end',
+                    alignItems: 'center',
+                    backgroundColor: 'transparent',
+                  }}
+                  onPress={()=>this.takePicture()}>
+                <FontAwesome
+                    name="camera"
+                    style={{ color: "#fff", fontSize: 40}}
+                />
+              </TouchableOpacity>
+              <TouchableOpacity
+                  style={{
+                    alignSelf: 'flex-end',
+                    alignItems: 'center',
+                    backgroundColor: 'transparent',
+                  }}
+                  onPress={()=>this.onPress()}>
+                <Ionicons
+                    name= 'ios-close-circle-outline'
+                    style={{ color: "#fff", fontSize: 40}}
+                />
+              </TouchableOpacity>
+            </View>
+          </Camera>
+        </View>
     )
   }
 }
